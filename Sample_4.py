e71fa77858cb13f2ec582517797bf314b6ffc2ca
Barxhe <anxiety135790@gmail.com>
2020-08-25 02:57:08 +0000

    init cmommit

--- Sample_4.py
+++ Sample_4.py
@@ -5,19 +5,17 @@ myd =input('YYYY/MM/DD:')
 print(type(myd))
 months = (0,31,59,90,120,151,181,212,243,273,304,334)
 
-# month = myd.split(0,3)
+myd =myd.split("/",-1)
+year = int(myd[0])
+month = int(myd[1])
+day = int(myd[2])
 
-# print(year)
-# print(month)
-# print(day)
-# # if 0 < month <= 12:
-#     sum = months[month - 1]
-# else:
-#     print 'data error'
-# sum += day
-# leap = 0
-# if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
-#     leap = 1
-# if (leap == 1) and (month > 2):
-#     sum += 1
-# print 'it is the %dth day.' % sum
+if 0 < month <= 12:
+    sum = months[month-1] + int(day)
+    if (year % 4 ==0) and (myd > 2):
+        sum  = months[month-1] + int(day)-1
+    else:
+        sum  = months[month-1] + int(day)
+else:
+    print('DATE ERROR')
+print(sum)
